8ad49d70cb5b44cd1dfbcf0be07ff5b78525d78c
Fred052 <ferid.suleymanzade05@gmail.com>
2024-03-25 07:21:53 +0300

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Ferid Suleymanzade on 25.03.24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
